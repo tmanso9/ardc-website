0b693ae62f6b2e8a08e5f613049cedd307a15ed8
tmanso9 <tmanso92@gmail.com>
2024-04-06 01:44:34 +0100

    jest adicionado ao lint-staged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,4 +1,5 @@
 module.exports = {
   '*.{js,jsx,ts,tsx,css}': 'eslint --config .eslintrc.js --fix',
+  '*.{js,jsx,ts,tsx,css}': 'jest --bail --findRelatedTests --passWithNoTests',
   '*': 'prettier --ignore-unknown --write --config .prettierrc'
 };
